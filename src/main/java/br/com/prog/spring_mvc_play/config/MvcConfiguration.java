5fa5f232460a6ecd020c12ef95ce30cfa447bfb1
isantos <isantos@localhost>
2018-03-14 18:47:39 -0300

    sistema teste Spring MVC

--- src/main/java/br/com/prog/spring_mvc_play/config/MvcConfiguration.java
+++ src/main/java/br/com/prog/spring_mvc_play/config/MvcConfiguration.java
@@ -1,13 +1,23 @@
 package br.com.prog.spring_mvc_play.config;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.ConversionService;
+import org.springframework.core.convert.support.DefaultConversionService;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import br.com.prog.spring_mvc_play.StringToEnumConverterFactory;
+import br.com.prog.spring_mvc_play.interceptor.AutorizadorInterceptor;
 
 @Configuration
 @ComponentScan(basePackages="br.com.prog.spring_mvc_play")
@@ -25,7 +35,32 @@ public class MvcConfiguration extends WebMvcConfigurerAdapter{
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+		registry.addResourceHandler("/resources/js/**").addResourceLocations("/resources/js/").setCachePeriod(31556926);
+	}
+	@Bean	
+	public ConversionService contaConversionService() {
+		DefaultConversionService service = new DefaultConversionService();
+		service.addConverterFactory(new StringToEnumConverterFactory());
+		return service;
 	}
 
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new AutorizadorInterceptor());
+	}
+	@Bean
+	public DataSource getDataSource() {
+		DriverManagerDataSource ds = new DriverManagerDataSource();
+		ds.setDriverClassName("org.hsqldb.jdbcDriver");
+		ds.setUsername("sa");
+		ds.setPassword("");
+		ds.setUrl("jdbc:hsqldb:file:contas.db");
+		return ds;
+	}
 	
 }
